08aba97a6cfad83bcde07d4da763a5e94d81cf18
NateInterloper <nalbertbnascimento@gmail.com>
2026-04-06 16:20:18 -0300

    minor: 'IconButton.js' faltava StyleSheet

--- praticas/pratica03/atividadeAvaliada/components/IconButton.js
+++ praticas/pratica03/atividadeAvaliada/components/IconButton.js
@@ -1,4 +1,4 @@
-import {Pressable, View} from 'react-native';
+import { StyleSheet, Pressable, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 function IconButton({ icon, size, color, onPress}) {
